d3b713984f5ae895d8fc2c6aff61c6958552fd64
iotconstruction1 <117975325+iotconstruction1@users.noreply.github.com>
2025-07-28 13:51:34 -0400

    Create App.js

---
+++ react-client/App.js
@@ -0,0 +1,9 @@
+const root = document.getElementById("root");
+
+root.innerHTML = `
+  <div class="container">
+    <h1>The Living Mirror</h1>
+    <p class="quote">“The mirror hears more than words…”</p>
+    <textarea placeholder="Speak to your mirror..."></textarea>
+  </div>
+`;
