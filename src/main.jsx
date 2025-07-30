2fa136fe773c49c55c0f38a18476028749b0b009
iotconstruction1 <117975325+iotconstruction1@users.noreply.github.com>
2025-07-29 20:32:50 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './styles/App.css'  // skip this line if you don’t have CSS yet
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
