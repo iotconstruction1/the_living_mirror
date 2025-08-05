44f198e053cd12e4bebab5bcfd2bbf0a49884a61
iotconstruction1 <117975325+iotconstruction1@users.noreply.github.com>
2025-08-05 17:00:08 -0400

    Update RealmCard.jsx

--- src/components/RealmCard.jsx
+++ src/components/RealmCard.jsx
@@ -4,7 +4,7 @@ export default function RealmCard({ data }) {
   return (
     <div className={`realm-card ${data.color}`}>
       <p className="tagline">{data.tagline}</p>
-      <div className="realm-icon">{data.icon}</div>
+      
       <h2 className="realm-name">{data.name}</h2>
       <button className="enter-button">Enter</button>
     </div>
