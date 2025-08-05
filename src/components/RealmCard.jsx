a74c361f0321d12f2b1886730c2cb2f9cad1c637
iotconstruction1 <117975325+iotconstruction1@users.noreply.github.com>
2025-08-05 16:47:35 -0400

    Update RealmCard.jsx

--- src/components/RealmCard.jsx
+++ src/components/RealmCard.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
 
+export default function RealmCard({ data }) {
+  return (
+    <div className={`realm-card ${data.color}`}>
+      <p className="tagline">{data.tagline}</p>
+      <div className="realm-icon">{data.icon}</div>
+      <h2 className="realm-name">{data.name}</h2>
+      <button className="enter-button">Enter</button>
+    </div>
+  );
+}
